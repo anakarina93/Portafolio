3fa4b7bfedbc3119004f144f4ed53e6a0cb319b7
anakarinacalero <anacalerova@gmail.com>
2025-06-05 16:30:04 -0400

    Agregar archivos de proyecto.

---
+++ Portafolio/Models/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portafolio.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your message")]
+
+        public string Message { get; set; }
+    }
+}
